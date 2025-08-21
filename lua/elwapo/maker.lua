a7a13363af7635b1c88d80c92089b04aa95f32ac
Sergio Madrid <smadrid062@gmail.com>
2025-08-21 08:43:46 -0600

    maker proto

--- lua/elwapo/maker.lua
+++ lua/elwapo/maker.lua
@@ -2,7 +2,7 @@ local function run_cmd(cmd)
   local args = type(cmd) == "string" and vim.split(cmd, "%s+") or cmd
   local proc = vim.system(args, { text = true })
   local result = proc:wait()
-  if result.code ~= 0 then
+  if result.code > 1 then
     return false, result.stderr
   end
   return true, result.stdout
@@ -16,22 +16,45 @@ local function is_real_target(t)
      and not t:match("/")          -- not a path
 end
 
-local function get_targets(arg_lead,cmd_line, cursor_pos)
-  local cmd = "make -qp"
-  local ok, targets = run_cmd(cmd)
-  if not ok then
-    vim.notify("unable to get make targets -> " .. targets, vim.log.levels.WARN)
-    return {}
-  end
-  local results = {}
-  for _, line in ipairs(vim.split(targets, "\n")) do
-    local target = line:match("^([%w_%.%-]+):")
-    if is_real_target(target) and target:lower():match("^" .. arg_lead:lower()) then
-      table.insert(results, target)
+local function cleanup(full)
+  local lines = vim.split(full, "\n")
+  local targets = {}
+  local seen = {}
+  for _,line in ipairs(lines) do
+    local target, _ = line:match("^([%w_%-/]+):%s*(.*)")
+    if target and target ~= "Makefile" then
+      if not seen[target] then
+        vim.notify(target)
+        seen[target] = true
+        table.insert(targets,target)
+      end
     end
   end
-  return results
+  return targets
 end
+
+local function get_targets()
+  local ok, out = run_cmd("make -qp -f Makefile")
+  if not ok then
+    vim.notify("error: "..out)
+    return
+  end
+  local  targets = cleanup(out)
+  return targets
+end
+
+--local function get_targets(arg_lead,cmd_line, cursor_pos)
+--  local cmd = "make -pRrq -f Makefile : 2>/dev/null"
+--  local ok, targets = run_cmd(cmd)
+--  if not ok then
+--    vim.notify("unable to get make targets -> " .. targets, vim.log.levels.WARN)
+--    return {}
+--  end
+--  targets = cleanup(targets)
+--  vim.notify(targets)
+--  local results = {"one" , "two" , "three"}
+--  return results
+--end
 
 local function make(target)
   local cmd = "make "..target
@@ -40,15 +63,17 @@ local function make(target)
     vim.notify("error running make ->"..error_msg, vim.log.levels.ERROR)
     return
   end
+  vim.notify("make "..target.." Succeded",vim.log.levels.Info)
 end
 
-vim.api.nvim_create_user_command("NMaker",
-function(opts)
-  local target = opts.args
-  make(target)
-end, {nargs = '?',
-      complete = get_targets})
-
-local M = {}
-M.make = make
-return M
+--vim.api.nvim_create_user_command("NMaker",
+--function(opts)
+--  local target = opts.args
+--  make(target)
+--end, {nargs = '?',
+--      complete = get_targets})
+--
+--local M = {}
+--M.make = make
+--return M
+get_targets()
