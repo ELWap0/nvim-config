e0debf26acf22d73fd628ebe7bcd72f5780b92df
Sergio Madrid <71784275+ELWap0@users.noreply.github.com>
2025-10-23 14:24:36 -0600

    Update mason.lua

--- lua/elwapo/plugins/mason.lua
+++ lua/elwapo/plugins/mason.lua
@@ -1,17 +1,26 @@
 return {
-  { 
-    "mason-org/mason.nvim",
-    lazy=false,
-    opts = {},
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    lazy=false,
-    opts = {
-      ensure_installed = { 
-        "lua_ls",
-      },
-      automatic_enable = true
-    }
-  }
+	{
+		"mason-org/mason.nvim",
+		lazy = false,
+		opts = {},
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			ensure_installed = {
+				"black",
+				"clang-format",
+				"clangd",
+				"dockerls",
+				"goimports",
+				"gopls",
+				"isort",
+				"lua_ls",
+				"pyright",
+				"stylua",
+			},
+			automatic_enable = true,
+		},
+	},
 }
