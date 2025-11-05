c4360b8b7b6a42a05eec79aed80806ce509b6032
Sergio Madrid <smadrid062@gmail.com>
2025-11-04 22:26:11 -0700

    deprication

--- lua/elwapo/config/lsp-config.lua
+++ lua/elwapo/config/lsp-config.lua
@@ -6,7 +6,7 @@ local signs = {
 }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl,{text=icon, texthl = hl, numhl=""})
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
 vim.diagnostic.config({
@@ -16,6 +16,25 @@ vim.diagnostic.config({
   underline = true,
   update_in_insert = true,
   severity_sort = true,
-  float = {border = "rounded"},
+  float = { border = "rounded" },
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = signs.Error,
+      [vim.diagnostic.severity.WARN] = signs.Warn,
+      [vim.diagnostic.severity.HINT] = signs.Hint,
+      [vim.diagnostic.severity.INFO] = signs.Info,
+    },
+    texthl = {
+      [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+      [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+      [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+      [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+    },
+    numhl = {
+      [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN] = "",
+      [vim.diagnostic.severity.HINT] = "",
+      [vim.diagnostic.severity.INFO] = "",
+    }
+  },
 })
-
